91ab83a965117d45cc28f61416d9c8f692736e7b
Aleksey Kabanov <aleksey.kabanov@gmail.com>
2022-07-21 17:48:48 +0300

    Add project files.

---
+++ Database1/dbo/Tables/clients.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[clients] (
+    [id]         INT           IDENTITY (1, 1) NOT NULL,
+    [name]       VARCHAR (100) NOT NULL,
+    [email]      VARCHAR (150) NOT NULL,
+    [phone]      VARCHAR (20)  NULL,
+    [address]    VARCHAR (100) NULL,
+    [created_at] DATETIME      DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    UNIQUE NONCLUSTERED ([email] ASC)
+);
+
